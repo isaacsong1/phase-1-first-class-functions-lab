a9fe68a894cd57590c3611b040cfd4ed51e94ab6
Isaac Song <isaacsongs10@gmail.com>
2023-09-11 22:22:49 -0700

    Advanced First-Class Functions Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+// Removes the first two elements in an array
+const returnFirstTwoDrivers = function (namesArr) {
+    return namesArr.slice(0, 2);
+}
+
+// Removes the last two elements in an array
+const returnLastTwoDrivers = function (namesArr) {
+    return namesArr.slice(namesArr.length - 2, namesArr.length);
+}
+
+// Array with last two functions
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// Higher-order function that returns a function to muliply a fare for a ride
+function createFareMultiplier(multiplierInt) {
+    return function (fareInt) {
+        return fareInt * multiplierInt;
+    }
+}
+
+// Variable with function above assigned to double fare
+const fareDoubler = createFareMultiplier(2);
+
+// Variable with function above assigned to triple fare
+const fareTripler = createFareMultiplier(3);
+
+// Function that takes in two arguments (namesArr and one of the first two functions) 
+// and returns either first or last two drivers
+function selectDifferentDrivers(namesArr, firstOrLastDriver) {
+    return firstOrLastDriver(namesArr);
+}
